77dc907846d851a99504403a4af0718f42be439d
Diana Gonzalez <dianagonzalez22.dg@gmail.com>
2016-05-25 09:39:46 -0300

    Add Basic StateMachine

--- models/game.js
+++ models/game.js
@@ -14,6 +14,73 @@ function Game(player1, player2){
 	this.player2 = new Player(player2, deck[3], deck[4], deck[5]);
 };
 
-
+var Player = playerModel.player;
+var Round  = roundModel.round;
+ 
+function Game(player1, player2){
+   /*
+    * Player 1
+    */
+   this.player1 = new Player('Player 1');
+ 
+   /*
+    * Player 2
+    */
+   this.player2 = new Player('Player 2');
+ 
+   /*
+    * sequence of previous Rounds
+    */
+   this.rounds = [];
+ 
+   /*
+    * Game's hand
+    */
+   this.currentHand = 'player1';
+ 
+   /*
+    * Game's hand
+    */
+   this.currentRound = undefined;
+ 
+   /*
+    * Game' score
+   */
+   this.score = [0, 0];
+ }
+ 
+ /*
+  * Check if it's valid move and play in the current round
+  */
+ Game.prototype.play = function(player, action, value){
+   if(this.currentRound.currentTurn !== player)
+     throw new Error("[ERROR] INVALID TURN...");
+ 
+   if(this.currentRound.fsm.cannot(action))
+     throw new Error("[ERROR] INVALID MOVE...");
+ 
+   return this.currentRound.play(action, value);
+ };
+ 
+ /*
+  * Create and return a new Round to this game
+  */
+ Game.prototype.newRound = function(){
+   var round = new Round(this, this.currentHand);
+   this.currentRound = round;
+   this.currentHand = switchPlayer(this.currentHand);
+   this.rounds.push(round);
+ 
+   return this;
+ }
+ 
+ /*
+  * returns the oposite player
+  */
+ function switchPlayer(player) {
+   return "player1" === player ? "player2" : "player1";
+ };
+ 
+ module.exports.game = Game;
 
 
--- models/player.js
+++ models/player.js
@@ -1,6 +1,55 @@
 /* Define the player model. */
+/*
+ * Represents a player in the game
+ * @param name [String]: old state to intialize the new state
+ */
+var _ = require('lodash');
 
-function Player(name, card1, card2, card3){ 
+function Player(name) {
+  /*
+   * the player's name
+   */
+ this.name = name;
+
+	/*
+	* cards of this user
+	*/
+	this.cards = [];
+
+ /*
+	* user envido points
+	*/
+	this.envidoPoints = 0;
+}
+
+/*
+ * Add cards to user and calculate the user points
+ */
+Player.prototype.setCards = function(cards){
+	this.cards = cards;
+	this.envidoPoints = this.points();
+}
+
+/*
+ * Returns the user envido points
+ */
+/* +Player.prototype.points = function(){
+ +  var pairs = [
+ +    [this.cards[0], this.cards[1]],
+ +    [this.cards[0], this.cards[2]],
+ +    [this.cards[1], this.cards[2]],
+ +  ];
+ +
+ +  var pairValues = _.map(pairs, function(pair){
+ +    return pair[0].envido(pair[1]);
+ +  });
+ +
+ +  return _.max(pairValues);
+ +};
+ +
+ + */
+
+/*function Player(name, card1, card2, card3){ 
 	this.name = name;
 	this.card1= card1;
 	this.card2= card2;
@@ -8,10 +57,10 @@ function Player(name, card1, card2, card3){
 	this.points = this.puntos();
 	console.log(this);
 };
-
+*/
 //Points
-Player.prototype.puntos = function(){
-	return Math.max(this.card1.puntos(this.card2), this.card1.puntos(this.card3), this.card2.puntos(this.card3));
+Player.prototype.points = function(){
+	return Math.max(this.cards[0].puntos(this.cards[1]), this.cards[0].puntos(this.cards[2]), this.cards[1].puntos(this.cards[2]));
 };
 
 module.exports.player = Player;
